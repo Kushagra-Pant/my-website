cb2a0ad42fb2f275d29d643f7b751fc7aece762f
Kushagra-Pant <1pantkus@gmail.com>
2025-10-26 13:23:56 -0400

    Fixed navbar issue for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Card from './Card/Card.tsx';
 import WideCard from './Card/WideCard.tsx';
-import NavBar from './NavBar/NavBar.tsx';
 // import Background from './Background/Background.tsx';
 
 function App() {
